4b651b1a047d6fa44f5e681dff2a5951989e831f
Kamal <93322430+kamalsq@users.noreply.github.com>
2021-11-11 12:29:28 +0530

    Update start_server.sh

--- script/start_server.sh
+++ script/start_server.sh
@@ -3,6 +3,7 @@ cd /home/ubuntu/node-express-realworld-example-app
 sudo apt update -y
 curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm install npm -g
 sudo apt install npm -y
 sudo npm install -d 
 sudo npm install pm2 -g
