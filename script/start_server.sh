b0a3040654b0ed4e6fad6725dffeb3ddd65dfee5
Kamal <93322430+kamalsq@users.noreply.github.com>
2021-11-23 18:59:48 +0530

    Update start_server.sh

--- script/start_server.sh
+++ script/start_server.sh
@@ -5,7 +5,9 @@ curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt install npm -y
 sudo npm install npm -g
-sudo npm install -d 
+sudo npm install -d
+sudo apt install screen -y
+sudo screen -R
 sudo nodejs app.js
 #sudo npm install pm2 -g
 #sudo pm2 start app.js
