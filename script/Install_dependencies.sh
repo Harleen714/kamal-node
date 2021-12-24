414860bc2be6e6c60e3614d83af04c441dd3c370
Kamal <93322430+kamalsq@users.noreply.github.com>
2021-12-24 18:49:52 +0530

    Create Install_dependencies.sh

---
+++ script/Install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ubuntu/node-express-realworld-example-app
+sudo apt update -y
+curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt install npm -y
+sudo npm install npm -g
+sudo npm install -d
+sudo npm install pm2 -g
