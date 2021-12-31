568c3e5d25f4cc1f4b10b12319ba942b61c16390
Kamal <93322430+kamalsq@users.noreply.github.com>
2021-12-31 12:45:02 +0530

    Update Install_dependencies.sh

--- script/Install_dependencies.sh
+++ script/Install_dependencies.sh
@@ -2,6 +2,7 @@
 sudo apt update -y
 curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
+cd /home/ubuntu/kamal-node/
 sudo apt install npm -y
 sudo npm install npm -g
 sudo npm install -d
